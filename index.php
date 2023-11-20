f8e591c32a894ef3dc7613fa15c7890f30bcd2a7
nl1352002 <112598962+nl1352002@users.noreply.github.com>
2023-11-20 12:51:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -233,6 +233,67 @@
                         location.reload();
                     })
                 }
+		 const APP_ID = 'cf26e7b2c25b5acd18ed5c3e836fb235';
+    const DEFAULT_VALUE = '--';
+    const searchInput = document.querySelector('#search-input');
+    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+
+    const recognition = new SpeechRecognition();
+    const synth = window.speechSynthesis;
+    recognition.lang = "vi-VI";
+    recognition.continuous = false;
+
+    const microphone = document.querySelector(".microphone");
+
+    const speak = (text) => {
+        if (synth.speaking) {
+            console.error("Busy. Speaking...");
+            return;
+        }
+
+        const utter = new SpeechSynthesisUtterance(text);
+
+        utter.onend = () => {
+            console.log("SpeechSynthesisUtterance.onend");
+        };
+        utter.onerror = (err) => {
+            console.error("SpeechSynthesisUtterance.onerror", err);
+        };
+
+        synth.speak(utter);
+    };
+
+    const handleVoice = (text) => {
+        console.log("text", text);
+        // Hiển thị đầu vào giọng nói được nhận dạng trong trường tìm kiếm
+        searchInput.value = text;
+        console.log("searchInput.value", searchInput.value);
+        // Tự động tải để tìm kiếm
+        const searchButton = document.querySelector(".search-btn");
+        searchButton.click();
+    };
+    microphone.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        recognition.start();
+        microphone.classList.add("recording");
+    });
+
+    recognition.onspeechend = () => {
+        recognition.stop();
+        microphone.classList.remove("recording");
+    };
+
+    recognition.onerror = (err) => {
+        console.error(err);
+        microphone.classList.remove("recording");
+    };
+
+    recognition.onresult = (e) => {
+        console.log("onresult", e);
+        const text = e.results[0][0].transcript;
+        handleVoice(text);
+    };
         </script>
 </body>
 </html>
